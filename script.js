bc81d70c319c91d26b1b5d44a9540fdd36773c76
fabi308 <fabiana10nasci@gmail.com>
2026-03-10 10:48:38 -0500

    att

---
+++ script.js
@@ -0,0 +1,47 @@
+ const lista = document.querySelector(".lista-produtos") //função do queryselector buscar um elemento//
+ 
+ const cafe = [
+    {
+        nome: "Espresso",
+        descricao: "Café concentrado y aromático",
+        preco: "$3.50",
+        img: "assets/espresso.jpg"
+    },
+
+     {
+        nome: "Capuccino",
+        descricao: "Espresso con leche vaporizada y espuma",
+        preco: "$4.50",
+        img: "assets/capuccino.jpg"
+    },
+
+    {
+        nome: "Café Helado",
+        descricao: "Refrescante café frío con hielo",
+        preco: "$5.00",
+        img: "assets/cafe-helado.jpg"
+    },
+ ]
+
+ const Repostería = [
+    {
+        nome: "Pastel de Chocolate",
+        descricao: "Delicioso pastel de chocolate casero",
+        preco: "$4.00",
+        img: "assets/pastel.jpg"
+    },
+
+     {
+        nome: "Croissant",
+        descricao: "Croissant francés recién horneado",
+        preco: "$3.00",
+        img: "assets/Croissant.jpg"
+    },
+
+    {
+        nome: "Muffin de Arándanos",
+        descricao: "Esponjoso muffin con arándanos frescos",
+        preco: "$3.50",
+        img: "assets/muffin.jpg"
+    },
+ ]
